7b0789d6566864e8284292a95f843c563bc27dd4
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-20 14:32:49 +0200

    Inizio Esercizio

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import inizio from './components/Header.vue';
+  import mezzo from './components/Main.vue';
+  import fine from './components/Footer.vue';
+  export default{
+    components: {
+      inizio,
+      mezzo,
+      fine
+    }
+  }
+</script>
+<template>
+  <div>
+    <inizio />
+    <mezzo />
+    <fine />
+  </div>
+</template>
+<style lang="scss">
+    @use './styles/generals.scss' as *;
+    @use './styles/partials/variables' as *;
+</style>
