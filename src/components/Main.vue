817cdc984c871acf64c5b72f2578a6007704e221
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-20 15:47:17 +0200

    Fatto main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,10 +4,22 @@
     }
 </script>
 <template>
-    <div>
-       
+    <div class="bg-black">
+        <div class="container">
+            <h2> -- Content goes here -- </h2>
+        </div>
     </div>
 </template>
-<style lang="scss">
-  
+<style lang="scss" scoped>
+    .bg-black{
+        background-color: #1c1c1c;
+    }
+    .container{
+        max-width: 1024px;
+        margin: 0 auto;
+        padding: 30px;
+        h2{
+            color: #fff;
+        }
+    }
 </style>
