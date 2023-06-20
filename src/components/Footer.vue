4172d4ff3a7fd5d53b182e9e193c5a73ea3a1f0a
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-20 17:55:38 +0200

    Fatto seconda parte del footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -32,7 +32,7 @@
 </script>
 <template>
     <div class="bg-blue">
-        <div class="container">
+        <div class="container1 content1">
             <a v-for="(image, index) in images" :key="index" href="">
                 <img :src="image.image" :class="{ 'svg-img': image.isSvg } ">
                 <span>{{ image.label }}</span>
@@ -40,12 +40,55 @@
         </div>
     </div>
     <div class="bg-image">
-        <div class="container">
-
+        <div class="container2 row">
+            <div class="col">
+                <div class="d-flex">
+                    <div>
+                        <h2>DC COMICS</h2>
+                        <p><a href="#">Characteres</a></p>
+                        <p><a href="#">Comics</a></p>
+                        <p><a href="#">Movies</a></p>
+                        <p><a href="#">Tv</a></p>
+                        <p><a href="#">Games</a></p>
+                        <p><a href="#">Videos</a></p>
+                        <p><a href="#">News</a></p>
+                        <h2>SHOP</h2>
+                        <p><a href="#">Shop DC</a></p>
+                        <p><a href="#">Shop DC Collectibles</a></p>
+                    </div>
+                    <div class="margin-left">
+                        <h2>DC</h2>
+                        <p><a href="#">Terms Of Use</a></p>
+                        <p><a href="#">Privacy policy (New)</a></p>
+                        <p><a href="#">Ad Choices</a></p>
+                        <p><a href="#">Advertising</a></p>
+                        <p><a href="#">Jobs</a></p>
+                        <p><a href="#">Subscriptions</a></p>
+                        <p><a href="#">Talent Workshops</a></p>
+                        <p><a href="#">CPSC Certificates</a></p>
+                        <p><a href="#">Ratings</a></p>
+                        <p><a href="#">Shop Help</a></p>
+                        <p><a href="#">Contact Us</a></p>
+                    </div>
+                    <div class="margin-left">
+                        <h2>SITES</h2>
+                        <p><a href="#">DC</a></p>
+                        <p><a href="#">MAD Magazine</a></p>
+                        <p><a href="#">DC Kids</a></p>
+                        <p><a href="#">DC Universe</a></p>
+                        <p><a href="#">DC Power Visa</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="image-container">
+                    <img src="../img/dc-logo-bg.png" alt="logo-bg">
+                </div>
+            </div>
         </div>
     </div>
     <div class="bg-grey">
-        <div class="container">
+        <div class="container3 content3">
 
         </div>
     </div>
@@ -65,9 +108,11 @@
         background-color: #303030;
     }
 
-    .container{
+    .container1, .container2, .container3{
         max-width: 1070px;
         margin: 0 auto;
+    }
+    .content1{
         display: flex;
         justify-content: center;
         align-items: center;
@@ -86,5 +131,43 @@
         .svg-img {
             width: 60px; /* Imposta la larghezza desiderata per l'immagine SVG */
         }
+    }
+    .row{
+        display: flex;
+        justify-content: space-between;
+        .col{
+            h2{
+                color: #fff;
+                font-size: 17px;
+                margin: 20px 0 10px 0;
+            }
+            p{
+                margin-bottom: 2px;
+            }
+            a{
+                color: grey;
+                text-decoration: none;
+                font-size: 13px;
+            }
+            .d-flex{
+                display: flex;
+                .margin-left{
+                    margin-left: 40px;
+                }
+            }
+        }
+        .image-container {
+            width: 100%;
+            height: 300px;
+            overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            object-fit: contain;
+            object-fit: cover;
+            img{
+                width: 70%;
+            }
+        } 
     }
 </style>
